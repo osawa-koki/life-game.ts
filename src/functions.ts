3f6a447cfa1aa977573e9bf136f6b99e68805025
osawa-koki <koki@osawa.dev>
2023-10-23 22:16:15 +0900

    ライフゲームの実装。

--- src/functions.ts
+++ src/functions.ts
@@ -1,3 +1,16 @@
-export function hello (): void {
-  console.log('Hello world!')
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+
+export function removeChildren (parent: string | HTMLElement): void {
+  const parentElement = typeof parent === 'string' ? document.getElementById(parent)! : parent
+  while (parentElement.firstChild != null) {
+    parentElement.removeChild(parentElement.firstChild)
+  }
 }
+
+export async function sleep (ms: number): Promise<void> {
+  await new Promise(resolve => setTimeout(resolve, ms))
+}
+
+export function emptyFunction (): void {}
+
+/* eslint-enable @typescript-eslint/no-non-null-assertion */
